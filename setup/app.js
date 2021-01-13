4806051aa091a018cf7aa23a21e5f3491f8ffe05
denis <mrisholigare@gmail.com>
2021-01-13 10:29:26 +0300

    javascript worked upon ,added open modal class from css

--- setup/app.js
+++ setup/app.js
@@ -8,10 +8,10 @@ const modalOverlay =document.querySelector(".modal-overlay");
 const closeBtn =document.querySelector(".close-btn");
 // adding an event listener
 modalBtn.addEventListener('click',function(){
-
+modalOverlay.classList.add("open-modal")
 
 })
 closeBtn.addEventListener('click',function(){
-
+modalOverlay.classList.remove("open-modal")
  
 })
